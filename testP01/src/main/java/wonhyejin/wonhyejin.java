b018e84a8a72aacfb4d5dca50348ee6ac746f012
sist <sist@DESKTOP-A7PK88B>
2022-03-18 15:40:03 +0900

    안녕하세요 원혜진이 작업함 자바 파일 및 html파일생성

---
+++ testP01/src/main/java/wonhyejin/wonhyejin.java
@@ -0,0 +1,10 @@
+package wonhyejin;
+
+public class wonhyejin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println(" 안녕하세요 원혜진이 작업함");
+	}
+
+}
