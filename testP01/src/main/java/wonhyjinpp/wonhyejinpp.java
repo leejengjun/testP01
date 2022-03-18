375c03552cb64245df8327eb534dea593e136db0
sist <sist@DESKTOP-A7PK88B>
2022-03-18 16:14:23 +0900

    내용변경

--- testP01/src/main/java/wonhyjinpp/wonhyejinpp.java
+++ testP01/src/main/java/wonhyjinpp/wonhyejinpp.java
@@ -5,6 +5,7 @@ public class wonhyejinpp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("dkssud");
+      System.out.println("dk내용변경d");
 	}
 
 }
