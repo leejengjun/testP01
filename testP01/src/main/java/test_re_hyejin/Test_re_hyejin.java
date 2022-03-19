fc4e6c51b5c55805ddcd79677f4ee2c60ec0276d
sist <sist@DESKTOP-A7PK88B>
2022-03-19 16:04:05 +0900

    개인브랜치에선 푸쉬가 되는데 메인브랜치에선 푸쉬가 안되서 다시해보기(내용추가 변경)

--- testP01/src/main/java/test_re_hyejin/Test_re_hyejin.java
+++ testP01/src/main/java/test_re_hyejin/Test_re_hyejin.java
@@ -4,7 +4,7 @@ public class Test_re_hyejin {
 
 	public static void main(String[] args) {
 		System.out.println("개인브랜치에선 푸쉬가 되는데 메인브랜치에선 푸쉬가 안되서 다시해보기");
-
+		System.out.println("개인브랜치에선 푸쉬가 되는데 메인브랜치에선 푸쉬가 안되서 다시해보기(변경된 내용추가 해봄");
 	}
 
 }
